41ba43885809af71afdd3e535d819b20d932ce28
Dinesh <dineshmoorthy101@gmail.com>
2022-07-29 10:32:53 +0530

    edited fileone.c for uploading it to github

--- fileone.c
+++ fileone.c
@@ -3,4 +3,6 @@
 int main()
 {
     printf("this is first remote file edited in github itself");
+    printf("Editing fileone.c (this file itself for using push command and uploading it github");
+    return 0;
 }
